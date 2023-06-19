4878a8279469a523bf1adba35ea322ff3b8fb3d4
Malte Heinzelmann <malte@cybaer.ninja>
2023-06-19 20:04:36 +0200

    Troopers changes

--- tools/flash.sh
+++ tools/flash.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-esptool.py --port /dev/ttyACM0 --baud 960000 write_flash 0x330000 appfs.bin
+esptool.py --port /dev/ttyUSB0 --baud 960000 write_flash 0x330000 appfs.bin
--- tools/generate.sh
+++ tools/generate.sh
@@ -3,5 +3,5 @@
 # Usage example
 
 python appfs_generate.py 8192000 appfs.bin
-python appfs_add_file.py appfs.bin ../Apps/badgePython/build/badge_firmware.bin python "BadgePython" 1
-python appfs_add_file.py appfs.bin ../Apps/ESP32\ binaries/gnuboy.bin gnuboy "GNUBOY Game Boy emulator" 1
+python appfs_add_file.py appfs.bin ../Apps/badgePython/build/badgepython.bin python "BadgePython" 1
+#python appfs_add_file.py appfs.bin ../Apps/ESP32\ binaries/gnuboy.bin gnuboy "GNUBOY Game Boy emulator" 1
