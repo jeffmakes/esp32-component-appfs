404b232c4baef55b021a142f445cc4fa89026d93
Malte Heinzelmann <malte@cybaer.ninja>
2023-06-26 21:29:34 +0200

    Added more apps

--- tools/generate.sh
+++ tools/generate.sh
@@ -4,4 +4,6 @@
 
 python appfs_generate.py 8192000 appfs.bin
 python appfs_add_file.py appfs.bin ../Apps/badgePython/build/badgepython.bin python "BadgePython" 1
+python appfs_add_file.py appfs.bin ../Apps/TROOPERSBATTLESHIP.bin python "Battleship" 2
+python appfs_add_file.py appfs.bin ../../../../mch2022-esp32-app-gnuboy/build/main.bin python "Gnuboy" 3
 #python appfs_add_file.py appfs.bin ../Apps/ESP32\ binaries/gnuboy.bin gnuboy "GNUBOY Game Boy emulator" 1
