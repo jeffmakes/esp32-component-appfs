cc1d75ebf6cf08f7171ad1203c6a996bb83483a2
Jeff Gough <j@jeffreygough.com>
2025-05-22 19:06:08 +0100

    Update generate script with correct paths for appfs binaries

--- tools/generate.sh
+++ tools/generate.sh
@@ -3,7 +3,7 @@
 # Usage example
 
 python appfs_generate.py 8192000 appfs.bin
-python appfs_add_file.py appfs.bin ../Apps/python_renze.bin python_tr23 "BadgePython" 1
-python appfs_add_file.py appfs.bin ../../../../tr23_badge_battleship/build/TROOPERSBATTLESHIP.bin battleship "Battleship" 2
-python appfs_add_file.py appfs.bin ../../../../mch2022-esp32-app-gnuboy/build/main.bin gnuboy_tr23 "Gnuboy" 3
+python appfs_add_file.py appfs.bin ../../../apps/badgepython/python_renze.bin python_tr23 "BadgePython" 1
+python appfs_add_file.py appfs.bin ../../../apps/battleship/TROOPERSBATTLESHIP.bin battleship "Battleship" 2
+python appfs_add_file.py appfs.bin ../../../apps/gnuboy/main.bin gnuboy_tr23 "Gnuboy" 3
 #python appfs_add_file.py appfs.bin ../Apps/ESP32\ binaries/gnuboy.bin gnuboy "GNUBOY Game Boy emulator" 1
